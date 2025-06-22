56366b4d49c709a5b9ca125e441ef8b745186cd1
Melad_Shawky <meladshawky577@gmail.com>
2025-06-22 19:53:59 +0300

    desing on boardin page part 1

---
+++ lib/core/resources/font_manger.dart
@@ -0,0 +1,5 @@
+class FontManger{
+  static const String orbitronFontName='Orbitron';
+  static const String orbitronFontPath='assets/fonts/orbitron/Orbitron-Regular.ttf';
+}
+
--- lib/features/on_boarding/screens/on_boarding_screen.dart
+++ lib/features/on_boarding/screens/on_boarding_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/core/resources/alignment_mangers.dart';
+import 'package:music_app/core/resources/assets_manger.dart';
+import 'package:music_app/core/resources/color_mangers.dart';
+import 'package:music_app/core/resources/font_manger.dart';
+import 'package:music_app/core/resources/height_values_mangers.dart';
+import 'package:music_app/core/resources/width_values_mangers.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(AlignmentMangersx.x0_20, AlignmentMangersy.y_0_90),
+            end: Alignment(AlignmentMangersx.x_0_20, AlignmentMangersy.y0_98),
+            colors: [
+              ColorMangers.kPrimaryColor,
+              ColorMangers.kSecandPrimaryColor.withOpacity(0.54),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            'Melo',
+            style: TextStyle(
+              fontFamily: FontManger.orbitronFontName,
+              fontSize: 50,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
